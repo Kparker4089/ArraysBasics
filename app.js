8a313d78217605f54de47a9aafe7f2cf41df548e
unknown <kparker4089@myhta.org>
2019-02-22 10:02:10 -1000

    completed arrays

--- app.js
+++ app.js
@@ -22,22 +22,29 @@ console.log(exArray[0]);
 
 
 //1. Create an array listing ingredients to your favorite recipe.  
-//Name your array the recipe name.
+//Name your array the recipe name. 
+var pavlova = ["milk", "eggs", "flour", "sugar", "egg beaters"];
 //2. Create an array listing the classes you are taking this semester.
 //Name your array "SpringSem19"
+var SpringSem19 = ["WebDev2", "Stem", "WorldHistory", "Algebra"];
 //3. Create an array listing your favorite all time movies.
 //Name your array "FaveMovies"
+var FaveMovies =["Fate of the furious", "Pele", "Clash of the Titans", "Wrath of the titans"];
 //4. Create an array listing any other "favorites" you have.
 //Name your array the category it is.
+var faveCelebs =["Neymar","Messi","Ronaldo"]
 //5. Access the second element in your recipe array and print it as part of the following sentence 
 //"The second ingredient to my favorite recipe is ________."
+console.log("The second ingredient to my favorite recipe is," +  pavlova[1]); 
 //6. Access the third element in your class list array and print it as part of the following sentence 
 //"One of the classes I'm taking this semester is ________."
+console.log("One of the classes I am taking this semester is" + SpringSem19[2])
 //7. Access the first element in your movie list array and print it as part of the following sentence 
 //"One of my favorite movies is ________."
+console.log("one of my favorite movies is" + FaveMovies[0])
 //8. Access the first element in your last  array and print it as part of the following sentence 
 //"One of my favorite __(fill in)__ is ________."
-
+console.log("one of my favorite celebrities is"+faveCelebs[1])
 for(var i = 1; i < 10; i = i +2){
     console.log("Hi everyone I can count to " + i);
 //var i = i+1;
